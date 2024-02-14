509f0f146ae12fb40b76dceacf06e1d69343ab50
Andrew Brower <abrower10612@gmail.com>
2024-02-13 17:12:41 -0700

    update flutter version to 3.16.9

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Download Flutter SDK for Linux
 FLUTTER_CHANNEL="stable"
-FLUTTER_VERSION="3.16.0"
+FLUTTER_VERSION="3.16.9"
 FLUTTER_SDK="flutter_linux_${FLUTTER_VERSION}-${FLUTTER_CHANNEL}.tar.xz"
 wget https://storage.googleapis.com/flutter_infra_release/releases/${FLUTTER_CHANNEL}/linux/${FLUTTER_SDK}
 
